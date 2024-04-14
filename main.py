f805c3eceb5dbbab3c1745f81d0692d467fcdfbf
bankirob <bankirob@mail.ru>
2024-04-14 08:01:56 +0500

    start normal commited project

--- main.py
+++ main.py
@@ -1,40 +1,86 @@
 import pandas as pd
 import streamlit as st
-import matplotlib.pyplot as plt
 import plotly.graph_objects as go
 
-# Загрузка данных и автоматическое кэширование с помощью st.cache
-@st.cache_data
+# Загрузка данных из CSV-файла
+@st.cache_resource
 def load_data():
-    return pd.read_csv("sallary2000-2023.csv")
+    """
+    Загрузка данных из CSV-файла.
 
-# Загрузка данных и кэширование
+    Returns:
+        DataFrame: DataFrame с данными о средней зарплате и инфляции.
+    """
+    return pd.read_csv("infl1404.csv")
+
+# Загрузка данных
 df = load_data()
 
-# Отображение данных
-st.write(df)
+# Преобразование данных для построения графика
+df = df.set_index('years')  # Устанавливаем годы как индекс
+df_transposed = df.T  # Транспонируем таблицу для удобства
 
+# 2.1. Сравнительный анализ средней зарплаты по секторам
 
-fig, ax = plt.subplots()
-ax.plot(df.columns[1:], df.iloc[0, 1:], label='Всего по экономике')
-ax.plot(df.columns[1:], df.iloc[1, 1:], label='Строительство')
-ax.plot(df.columns[1:], df.iloc[2, 1:], label='Финансовая деятельность')
-ax.plot(df.columns[1:], df.iloc[3, 1:], label='Образование')
-ax.set_xlabel('Год')
-ax.set_ylabel('Значение')
-ax.set_title('Динамика данных по годам')
-ax.legend()
-st.pyplot(fig)
-
+# Создание интерактивного графика
 fig = go.Figure()
 
-fig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[0, 1:], mode='lines', name='Всего по экономике'))
-fig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[1, 1:], mode='lines', name='Строительство'))
-fig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[2, 1:], mode='lines', name='Финансовая деятельность'))
-fig.add_trace(go.Scatter(x=df.columns[1:], y=df.iloc[3, 1:], mode='lines', name='Образование'))
+# Добавляем данные о средних зарплатах в каждой сфере на график
+fig.add_trace(go.Scatter(x=df_transposed.index, y=df_transposed['construction_avg_salary_inflation_adjusted_2000'], mode='lines', name='Construction'))
+fig.add_trace(go.Scatter(x=df_transposed.index, y=df_transposed['finance_avg_salary_inflation_adjusted_2000'], mode='lines', name='Finance'))
+fig.add_trace(go.Scatter(x=df_transposed.index, y=df_transposed['education_avg_salary_inflation_adjusted_2000'], mode='lines', name='Education'))
 
-fig.update_layout(title='Динамика данных по годам',
-                  xaxis_title='Год',
-                  yaxis_title='Значение')
+# Настройка внешнего вида графика
+fig.update_layout(
+    title='Средняя зарплата с учетом инфляции (2000-2023)',
+    xaxis_title='Год',
+    yaxis_title='Зарплата',
+    legend_title='Сектор',
+    xaxis=dict(tickangle=45),
+    yaxis=dict(gridcolor='lightgray'),
+    hovermode='x'
+)
 
+# Отображение графика
 st.plotly_chart(fig)
+
+# 2.2. Динамика общей средней зарплаты
+
+# Создание графика
+total_salary_fig = go.Figure()
+
+# Добавляем данные о средней зарплате в общем
+total_salary_fig.add_trace(go.Scatter(x=df.columns[1:], y=df.loc['total'][1:], mode='lines', name='Total Salary'))
+
+# Настройка внешнего вида графика
+total_salary_fig.update_layout(
+    title='Средняя зарплата (2000-2023)',
+    xaxis_title='Год',
+    yaxis_title='Зарплата',
+    xaxis=dict(tickangle=45),
+    yaxis=dict(gridcolor='lightgray'),
+    hovermode='x'
+)
+
+# Отображение графика
+st.plotly_chart(total_salary_fig)
+
+# Создание графика
+inflation_fig = go.Figure()
+
+# Добавляем данные об инфляции
+inflation_fig.add_trace(go.Scatter(x=df.columns[1:], y=df.loc['inflation_year'][1:], mode='lines', name='Inflation'))
+
+# Настройка внешнего вида графика
+inflation_fig.update_layout(
+    title='Уровень инфляции (2001-2023)',
+    xaxis_title='Год',
+    yaxis_title='Уровень инфляции',
+    xaxis=dict(tickangle=45),
+    yaxis=dict(gridcolor='lightgray'),
+    hovermode='x'
+)
+
+# Отображение графика
+st.plotly_chart(inflation_fig)
+
